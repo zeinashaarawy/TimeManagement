f7e08d40c02ded5be4e2f1005ccc4e434925ca59
zeinashaarawy <zeinashaarwy2@gmail.com>
2025-11-12 23:52:42 +0200

    feat(setup): Integrated backend code and finalized folder structure.

---
+++ backend/src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
+  ],
+})
+export class AppModule {}
