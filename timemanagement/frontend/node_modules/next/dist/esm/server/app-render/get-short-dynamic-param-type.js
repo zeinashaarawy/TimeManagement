3afa8df3f4aa8bd6b4a873f86d7bdc7cd1e28593
zeinashaarawy <zeinashaarwy2@gmail.com>
2025-11-17 20:21:45 +0200

    Remove node_modules from Git tracking

--- timemanagement/frontend/node_modules/next/dist/esm/server/app-render/get-short-dynamic-param-type.js
+++
@@ -1,9 +0,0 @@
-export const dynamicParamTypes = {
-    catchall: 'c',
-    'catchall-intercepted': 'ci',
-    'optional-catchall': 'oc',
-    dynamic: 'd',
-    'dynamic-intercepted': 'di'
-};
-
-//# sourceMappingURL=get-short-dynamic-param-type.js.map
